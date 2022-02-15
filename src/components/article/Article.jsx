a2d385f63ff3edf19e1a43b71c56315c12f6deb4
Henri Bresteau <henri.bresteau@gamil.com>
2022-02-15 16:10:12 +0100

    structure components

---
+++ src/components/article/Article.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./article.css";
+
+const Article = () => {
+  return <div>articles</div>;
+};
+
+export default Article;
---
+++ src/components/navbar/Nabavar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./navbar.css";
+import {RiMenu3Line} from "react-icons/ri"
+
+const Nabavar = () => {
+  return <div className="gtp3__navbar">
+    <div className="gtp3__navbar-links">
+      <div className="gpt3__navbar-links_logo">
+        <img src={logo} alt="logo" />
+      </div>
+    </div>
+  </div>;
+};
+
+export default Nabavar;
