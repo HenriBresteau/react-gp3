06ffb90260c2b2a00b88d97b7d85d82620ebcbdd
Henri Bresteau <henri.bresteau@gamil.com>
2022-02-23 15:45:39 +0100

    sections id

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -4,7 +4,7 @@ import possibiltyImage from "../../assets/possibility.png";
 
 const Possibility = () => {
   return (
-    <div className="gpt3__possibility section__padding">
+    <div className="gpt3__possibility section__padding" id="possibility">
       <div className="gpt3__possibility-image">
         <img src={possibiltyImage} alt="possibilities" />
       </div>
